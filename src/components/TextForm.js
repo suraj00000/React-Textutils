bead8e38ac14fdb9cf9ac53c32ee4c5a7892d90d
Suraj kushwaha <skk74143@gmail.com>
2022-01-22 14:44:39 +0530

    Add some more feature in textutils

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,20 +7,34 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText);
     }
+    const handleLowClick =()=>{        
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
     const handleOnChange =(event)=>{
         // console.log("Change");
         setText(event.target.value)
     }
-    const [text,setText]=useState('Enter text here 2');
+    const [text,setText]=useState('');
     // text = "New text"; // Wrong way to change the text
     // setText("");// correct way to cghange the text
     return (
-        <div>
+        <>
+        <div className='container'>            
             <h1>{props.heading}</h1>
             <div className="mb-3">             
-                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" placeholder='Enter Text here' value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to upperCase</button>
+            <button className="btn btn-primary mx-1" onClick={handleUpClick}>UPPERCASE</button>
+            <button className="btn btn-primary mx-1" onClick={handleLowClick}>lowercase</button>
         </div>
+        <div className="container my-4">
+            <h1>Your text summery</h1>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.008*text.split(" ").length} Minutes to read </p>
+            <h3> Preview </h3>
+            <p>{text}</p>
+        </div>
+        </>
     )
 }
